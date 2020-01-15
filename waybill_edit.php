da71891c800632c39cf3e65946806ca4a3431e9a
maneerat jeenduang <jijanalakza55@gmail.com>
2020-01-15 19:57:33 +0700

    1.0.1

--- waybill_edit.php
+++ waybill_edit.php
@@ -6,7 +6,7 @@ function Editdata($row_waybill,$customer){
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+<title>แก้ไข</title>
 </head>
 
 <body>
